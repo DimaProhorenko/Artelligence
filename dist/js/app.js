cc766db0498a62ad1a823ce559463eb6a3a22eb6
DimaProhorenko <dima.study.web@gmail.com>
2023-03-12 00:20:51 +0200

    hero section and burger animation

--- dist/js/app.js
+++ dist/js/app.js
@@ -2,5 +2,6 @@ const burgerBtn = document.querySelector('.header__burger-button');
 const mainNav = document.querySelector('.header__nav');
 
 burgerBtn.addEventListener('click', e => {
+    burgerBtn.classList.toggle('active');
     mainNav.classList.toggle('active')
 })
