d0f24b8baa463f862c2b88f1a0601803d9c5405a
DimaProhorenko <dima.study.web@gmail.com>
2023-03-12 18:17:12 +0200

    finished study section

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,7 +1,23 @@
+
 const burgerBtn = document.querySelector('.header__burger-button');
 const mainNav = document.querySelector('.header__nav');
 
 burgerBtn.addEventListener('click', e => {
     burgerBtn.classList.toggle('active');
     mainNav.classList.toggle('active')
+});
+
+
+// Splide
+document.addEventListener('DOMContentLoaded', e => {
+    const testimonialsSplide = new Splide('.testimonials__slider', {
+        type: 'slide',
+        rewind: true,
+        gap: '3em',
+        autoplay: false,
+        pause: false,
+        interval: 2000, 
+        pauseOnHover: true,
+    });
+    testimonialsSplide.mount();
 })
